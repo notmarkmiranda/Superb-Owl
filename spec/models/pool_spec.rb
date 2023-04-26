13cd83b8408754a5573460546467a9f458a02bfd
Mark Miranda <mmiranda@doximity.com>
2023-04-25 22:00:07 -0600

    Fix pool model spec

--- spec/models/pool_spec.rb
+++ spec/models/pool_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Pool, type: :model do
   it { should belong_to :super_admin }
-  it { should have_many :memberships }
+  it { should have_many :admin_memberships }
+  it { should have_many(:admins).through(:admin_memberships) }
+  it { should have_many :member_memberships }
+  it { should have_many(:members).through(:member_memberships) }
   it { should define_enum_for(:game).with_values([:prop_bet]) }
 end
